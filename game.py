23c682989481da269f6d92ac3b34fb4ad537df5a
1RoBe <58035534+1RoBe@users.noreply.github.com>
2024-01-21 17:11:27 +0100

    Revert "tried to implement a buffer that checks if new movement = self.body[1] position to prevevne r l u d movement"
    This reverts commit 6a955bb902aa313aef346d3d70edbd52befcc97b.

--- game.py
+++ game.py
@@ -23,8 +23,7 @@ def main():
     FIELD_MAX_Y = TILE_MAX_Y * TILE_SIZE + FIELD_MIN_Y
     FIELD_DIMENSION: list[list[int], list[int]] = [[FIELD_MIN_X, FIELD_MAX_X], [FIELD_MIN_Y, FIELD_MAX_Y]]
     
-    SNAKE_STARTING_POSITION = [int(TILE_MAX_X/2), int(TILE_MAX_Y/2)]
-    
+
     SCREEN_WIDTH = FIELD_MIN_X + (TILE_MAX_X + 1) * TILE_SIZE + FIELD_MIN_Y
     SCREEN_HEIGHT = (TILE_MAX_Y + 1) * TILE_SIZE
 
@@ -47,7 +46,7 @@ def main():
                   field_dimension = FIELD_DIMENSION,
                   tile_dimension = TILE_DIMENSION,
                   tile_size = TILE_SIZE,
-                  tile_position = SNAKE_STARTING_POSITION,
+                  tile_position = [0, 0],
                   direction = 2)
 
     fruit = Fruit(screen = screen, 
